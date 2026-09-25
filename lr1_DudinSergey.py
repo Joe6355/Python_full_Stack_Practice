e84bc993ce5a9a3338eab7df5c40711fc07c4eaf
Joe6355 <115397312+Joe6355@users.noreply.github.com>
2026-09-25 16:56:48 +0500

    Сделал:
    Added a multiline print statement with a poem in Russian.

--- lr1_DudinSergey.py
+++ lr1_DudinSergey.py
@@ -2,5 +2,9 @@ def main():
     print("Dudin Sergey")
     print("ivt-ivs-203B - first subgroup ")
     print("25.09.26")
+    print(""Я вдыхаю этот воздух\n
+    Солнце в небе смотрит на меня\n
+    Надо мною летает вольный ветер\n
+    Он такой же как и я)\n
 if __name__ == '__main__':
     main()
